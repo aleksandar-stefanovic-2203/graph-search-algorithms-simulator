d4874e8a5ff7233f2840527a7a65158198c6af3b
aleksandar-stefanovic-2203 <aleksandar.stefanovic2203@gmail.com>
2026-04-08 00:32:30 +0200

    refactor: renamed IOTest.java to JSONIOTest.java

--- src/test/java/io/JSONIOTest.java
+++ src/test/java/io/JSONIOTest.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class IOTest {
+class JSONIOTest {
 
 	private static void assertGraphEquals(Graph expected, Graph actual) {
 		assertEquals(expected.getAdjacencyList().keySet(), actual.getAdjacencyList().keySet());
